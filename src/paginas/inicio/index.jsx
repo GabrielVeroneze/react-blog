62cd004856baafde148b6f3bfa966084bd590f81
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-07-28 23:00:59 -0300

    :sparkles: feat: Adiciona uma lista com os posts

--- src/paginas/inicio/index.jsx
+++ src/paginas/inicio/index.jsx
@@ -1,9 +1,16 @@
 import Banner from "componentes/Banner"
+import Post from "componentes/Post"
+import styles from './Inicio.module.scss'
+import posts from 'json/posts.json'
+
 
 const Inicio = () => {
     return (
-        <main>
+        <main className={styles.principal}>
             <Banner />
+            <ul className={styles.posts}>
+                {posts.map(post => <Post key={post.id} {...post} />)}
+            </ul>
         </main>
     )
 }
