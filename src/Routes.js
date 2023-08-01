bcbdb9ab1a98a4ea3dc52610ae2153dacae09dbf
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-08-01 20:28:19 -0300

    :sparkles: feat: Utiliza a prop index
    Utiliza a prop index para estabelecer a rota inicio como a rota padrão

--- src/Routes.js
+++ src/Routes.js
@@ -14,8 +14,8 @@ function AppRoutes() {
             <Menu />
             <Routes>
                 <Route path='/' element={<EstruturaPadrao />}>
-                    <Route path='/' element={<Inicio />}  />    
-                    <Route path='/sobremim' element={<SobreMim />} />
+                    <Route index element={<Inicio />}  />    
+                    <Route path='sobremim' element={<SobreMim />} />
                 </Route>
                 <Route path='*' element={<div>Página não encontrada</div>}/>
             </Routes>         
