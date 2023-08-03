55b7b44e01d5d1db22d877042382feba380d18b5
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-08-03 16:51:23 -0300

    :sparkles: feat: Adiciona destinos no Link
    Adiciona destino dinâmico nos links dos cards para seu post correspondente, baseado em seu id

--- src/componentes/PostCard/index.jsx
+++ src/componentes/PostCard/index.jsx
@@ -1,17 +1,19 @@
-import Botao from 'componentes/Botao'
 import styles from './PostCard.module.scss'
+import { Link } from 'react-router-dom'
 
 const PostCard = ({ id, titulo }) => {
     return (
         <li className={styles.card}>
-            <img 
+            <img
                 className={styles.card__capa}
                 src={`/assets/posts/${id}/capa.png`}
                 alt="Imagem de capa do post"
             />
             <div className={styles.card__conteudo}>
                 <h2 className={styles.card__titulo}>{titulo}</h2>
-                <Botao texto='Ler' />
+                <Link className={styles.card__botao} to={`/posts/${id}`}>
+                    Ler
+                </Link>
             </div>
         </li>
     )
